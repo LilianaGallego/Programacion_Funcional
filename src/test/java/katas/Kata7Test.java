33a420debad6e154eaa301827f1c5e0381235621
dsbpajedrez <96492716+dsbpajedrez@users.noreply.github.com>
2023-01-11 18:52:28 -0500

    Retrieve the id, title, and smallest box art url for every video

--- src/test/java/katas/Kata7Test.java
+++ src/test/java/katas/Kata7Test.java
@@ -1,7 +1,13 @@
 package katas;
 
+import com.google.common.collect.ImmutableMap;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 
@@ -11,5 +17,15 @@ public class Kata7Test {
     @Test
     public void testExecute() {
         Assert.assertThat(Kata7.execute(), equalTo(4));
+    }
+
+    @Test
+    public void testResult() {
+        List<Map> videos = new ArrayList<>();
+        videos.add(ImmutableMap.of("id", 70111470, "title", "Die Hard", "url", Optional.of("http://cdn-0.nflximg.com/images/2891/DieHard150.jpg")));
+        videos.add(ImmutableMap.of("id", 654356453, "title", "Bad Boys", "url", Optional.of("http://cdn-0.nflximg.com/images/2891/BadBoys150.jpg")));
+        videos.add(ImmutableMap.of("id", 65432445, "title", "The Chamber", "url", Optional.of("http://cdn-0.nflximg.com/images/2891/TheChamber150.jpg")));
+        videos.add(ImmutableMap.of("id", 675465, "title", "Fracture", "url", Optional.of("http://cdn-0.nflximg.com/images/2891/Fracture150.jpg")));
+        Assert.assertThat(Kata7.execute(), equalTo(videos));
     }
 }
