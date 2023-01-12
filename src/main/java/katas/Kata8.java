bc140f2b1f81883c385200a0bb9457799398edac
dsbpajedrez <96492716+dsbpajedrez@users.noreply.github.com>
2023-01-11 20:11:56 -0500

    new: Combine videos and bookmarks by index (StreamUtils.zip) (https://github.com/poetix/protonpack)

--- src/main/java/katas/Kata8.java
+++ src/main/java/katas/Kata8.java
@@ -10,6 +10,8 @@ import util.DataUtil;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /*
     Goal: Combine videos and bookmarks by index (StreamUtils.zip) (https://github.com/poetix/protonpack)
@@ -21,8 +23,15 @@ public class Kata8 {
         List<Movie> movies = DataUtil.getMovies();
         List<Bookmark> bookMarks = DataUtil.getBookMarks();
 
-        // StreamUtils.zip()
+        Stream<Integer> moviesList = movies.stream()
+                .map(Movie::getId);
+        Stream<Integer> bookMarksList = bookMarks.stream()
+                .map(Bookmark::getId);
 
-        return ImmutableList.of(ImmutableMap.of("videoId", 5, "bookmarkId", 3));
+        List<Map> moviesCombined = StreamUtils.zip(moviesList,bookMarksList,
+                (m,b)-> ImmutableMap.of("idMovi", m, "idBookMark", b))
+                        .collect(Collectors.toList());
+
+        return moviesCombined;
     }
 }
