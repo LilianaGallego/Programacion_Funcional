ad6814dcd96cc9129d147b932399d7dd05a8770a
dsbpajedrez <96492716+dsbpajedrez@users.noreply.github.com>
2023-01-14 15:27:49 -0500

    New: Tests para los puntos 8 ,9,10 y 11

--- src/test/java/katas/Kata8Test.java
+++ src/test/java/katas/Kata8Test.java
@@ -1,7 +1,12 @@
 package katas;
 
+import com.google.common.collect.ImmutableMap;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 
@@ -9,7 +14,17 @@ import static org.hamcrest.CoreMatchers.equalTo;
 public class Kata8Test {
 
     @Test
-    public void testExecute() {
-        Assert.assertThat(Kata8.execute(), equalTo(4));
+    public void testSize() {
+        Assert.assertThat(Kata8.execute().size(), equalTo(3));
     }
+
+    @Test
+    public void testResult() {
+        List<Map> videoBookmarks = new ArrayList<>();
+        videoBookmarks.add(ImmutableMap.of("videoId", 70111470, "bookmarkId", 470));
+        videoBookmarks.add(ImmutableMap.of("videoId", 654356453, "bookmarkId", 453));
+        videoBookmarks.add(ImmutableMap.of("videoId", 65432445, "bookmarkId", 445));
+        Assert.assertThat(Kata8.execute(), equalTo(videoBookmarks));
+    }
+
 }
